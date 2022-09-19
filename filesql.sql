e917ede7bed8c285a3c70a2b8b1f04b37894be23
Rstrydomjnr <113905009+Rstrydomjnr@users.noreply.github.com>
2022-09-19 16:54:05 +0200

    Update filesql.sql

--- filesql.sql
+++ filesql.sql
@@ -1,3 +1,6 @@
 SElECT * FROM users
 --WHERE email ilike '%system%'
-WHERE id = '612b5857-ca72-4f1b-a5ff-732158c02346'
+WHERE id = '612b5857-ca72-4f1b-a5ff-732158c02346';
+
+
+SELECT * FROM policy_update_requests where policy_id = '7ad70031-d5b2-4f36-9528-e6a8c76cec6c';
